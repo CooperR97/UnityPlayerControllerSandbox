82f332012a47f4d95388682f5f310657954f5fd9
Cooper Redfern <coopr97@gmail.com>
2019-10-03 22:35:56 -0500

    intial commit

---
+++ iDontKnOwWhaaTThisIS/Assets/Scripts/RigidBodyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class RigidBodyController : MonoBehaviour
+{
+    public Slider rb2dSlider;
+    public Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        rb2dSlider = gameObject.GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        //rb2d.mass = rb2dSlider.value;
+        rb2d.gravityScale = rb2dSlider.value;
+    }
+}
