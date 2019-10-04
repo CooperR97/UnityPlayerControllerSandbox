04dbf1751e17f192f018489c5589a30067139d70
Cooper Redfern <coopr97@gmail.com>
2019-10-04 09:19:26 -0500

    addedSliders

---
+++ iDontKnOwWhaaTThisIS/Assets/Scripts/DragSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragSlider : MonoBehaviour
+{
+
+    public Slider rb2dSlider;
+    public Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        rb2dSlider = gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeLinearDrag();
+    }
+
+    public void ChangeLinearDrag()
+    {
+        rb2d.drag = rb2dSlider.value;
+    }
+}
--- iDontKnOwWhaaTThisIS/Assets/Scripts/RigidBodyController.cs
+++ iDontKnOwWhaaTThisIS/Assets/Scripts/RigidBodyController.cs
@@ -8,15 +8,32 @@ public class RigidBodyController : MonoBehaviour
     public Slider rb2dSlider;
     public Rigidbody2D rb2d;
 
+
     private void Start()
     {
         rb2d = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         rb2dSlider = gameObject.GetComponent<Slider>();
+
     }
 
     private void Update()
     {
         //rb2d.mass = rb2dSlider.value;
-        rb2d.gravityScale = rb2dSlider.value;
+        //rb2d.gravityScale = rb2dSlider.value;
+    }
+
+    public void ChangeMass(float sld)
+    {
+        rb2d.mass = sld;
+    }
+
+    public void ChangeGravity(float sld)
+    {
+        rb2d.gravityScale = sld;
+    }
+
+    public void ChangeLinearDrag(float sld)
+    {
+        rb2d.drag = sld;
     }
 }
